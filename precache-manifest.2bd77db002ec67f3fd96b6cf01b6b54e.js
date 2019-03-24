f708192b2af7de17609d814fd6d4b1c74e5406c2
Veera Reddy Obula <veerareddy.obula@gmail.com>
2019-03-24 23:52:27 +0530

    Updates

---
+++ precache-manifest.2bd77db002ec67f3fd96b6cf01b6b54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e93c1e0c5005d77cf3c",
+    "url": "/static/js/main.5a85f822.chunk.js"
+  },
+  {
+    "revision": "0598bde7d96ad944659d",
+    "url": "/static/js/2.6c6d9e84.chunk.js"
+  },
+  {
+    "revision": "0e93c1e0c5005d77cf3c",
+    "url": "/static/css/main.03dab7b2.chunk.css"
+  },
+  {
+    "revision": "0598bde7d96ad944659d",
+    "url": "/static/css/2.ca5eef65.chunk.css"
+  },
+  {
+    "revision": "e0ecbfc9c59d21017c09b47871169b93",
+    "url": "/index.html"
+  }
+];
